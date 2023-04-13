94ad381e379d677d76a618a6f87e5359bc96cd61
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-13 13:15:59 -0400

    [Node] added class for XYTheta node to keep Node base class generalized

--- Node.py
+++ Node.py
@@ -2,10 +2,16 @@ import numpy as np
 
 
 class Node:
-    def __init__(self, x, y, yaw):
-        self.x = x
-        self.y = y
-        self.yaw = yaw
+    def __init__(self, state):
+        self.state = state
         self.parent = None
         self.cost_to_come = np.inf
         self.path_to_parent = None
+
+
+class XYThetaNode(Node):
+    def __init__(self, state):
+        super().__init__(state)
+        self.x = state[0]
+        self.y = state[1]
+        self.yaw = state[2]
