d950bfc6bff0732647cdec4d803ca4f179a13cab
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-14 19:34:53 -0400

    [run] updating run file to test obstacle checking

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from RRTBase import *
+import numpy as np
 
 node = XYThetaNode([0, 0, 0])
-circ_obs = CircularObstacle(1, 1, 2)
-rect_obs = AxisAlignedRectObstacle(-1, -1, 2, 2)
+circ_obs = CircularObstacle(1, 1, 0.1)
+rect_obs = AxisAlignedRectObstacle(-1, -1, 1, 2)
 
 print(f'node is within circular obstacle: {circ_obs.is_within_obs(node)}')
 print(f'node is within rectangular obstacle: {rect_obs.is_within_obs(node)}')
+
+obs_list = [circ_obs, rect_obs]
+print(f'{np.any([obstacle.is_within_obs(node) for obstacle in obs_list])}')
