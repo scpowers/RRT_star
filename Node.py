0569da3cdcde324bf21c87504f23edb4dcd74296
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-14 19:05:09 -0400

    [Node] switched base class to require x and y, real flexibility comes after in terms of what state variables you want, but this is for vehicles

--- Node.py
+++ Node.py
@@ -7,38 +7,41 @@ class Node(ABC):
     Base class for nodes.
 
     ** Attributes **
-        state: list of state variables corresponding to this node
+        x: x-coordinate of vehicle
+        y: y-coordinate of vehicle
         parent: Node object that is this node's parent in the tree
         cost_to_come: cost to travel to this node via its parents from the root
         path_to_parent: array(s) of points describing the trajectory from the parent to this node in state space
     """
-    def __init__(self, state):
+    def __init__(self, x, y):
         """
         Constructor for general node.
 
         Args:
-            state: **list**
-                list of state variables
+            x: **double or int**
+                x-coordinate of vehicle
+            y: **double or int**
+                y-coordinate of vehicle
         """
-        self.state = state
+        self.x = x
+        self.y = y
         self.parent = None
         self.cost_to_come = np.inf
         self.path_to_parent = None
 
 
 class XYThetaNode(Node):
-    def __init__(self, state):
+    def __init__(self, x, y, theta):
         """
         Constructor for x-y-theta node.
 
         Args:
-            state: **list, double or int**
-                list of state variables in the following order:
-                    1. x
-                    2. y
-                    3. theta/yaw (heading on x-y plane)
+            x: **double or int**
+                x-coordinate of vehicle
+            y: **double or int**
+                y-coordinate of vehicle
+            theta: **double or int**
+                heading of vehicle on x-y plane (in radians)
         """
-        super().__init__(state)
-        self.x = state[0]
-        self.y = state[1]
-        self.yaw = state[2]
+        super().__init__(x, y)
+        self.yaw = theta
