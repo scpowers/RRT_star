22f9b05a08d754652ee8b57070753c8f76915045
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-14 16:03:48 -0400

    [Node] actually adding changes

--- Node.py
+++ Node.py
@@ -2,16 +2,10 @@ import numpy as np
 
 
 class Node:
-    def __init__(self, state):
-        self.state = state
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.yaw = theta
         self.parent = None
         self.cost_to_come = np.inf
         self.path_to_parent = None
-
-
-class XYThetaNode(Node):
-    def __init__(self, state):
-        super().__init__(state)
-        self.x = state[0]
-        self.y = state[1]
-        self.yaw = state[2]
