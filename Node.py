1d988dcb65fc1d30976677e41f3e94623dd474bf
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-14 16:32:10 -0400

    [Node] keeping it generalized for RRT definition

--- Node.py
+++ Node.py
@@ -1,11 +1,18 @@
 import numpy as np
+from abc import ABC
 
 
-class Node:
-    def __init__(self, x, y, theta):
-        self.x = x
-        self.y = y
-        self.yaw = theta
+class Node(ABC):
+    def __init__(self, state):
+        self.state = state
         self.parent = None
         self.cost_to_come = np.inf
         self.path_to_parent = None
+
+
+class XYThetaNode(Node):
+    def __init__(self, state):
+        super().__init__(state)
+        self.x = state[0]
+        self.y = state[1]
+        self.yaw = state[2]
