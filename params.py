8f48112888838379828188df478094e42942d4ee
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-15 15:36:02 -0400

    [params] added steer params

--- params.py
+++ params.py
@@ -1,8 +1,14 @@
 import numpy as np
 
+# parameters for sampling nodes
 MAP_X_MIN = 0
 MAP_X_MAX = 10
 MAP_Y_MIN = 0
 MAP_Y_MAX = 10
 YAW_MIN = -np.pi
 YAW_MAX = np.pi
+
+# steer function parameters
+MIN_STEP = 0.2
+MAX_STEP = 3
+YAW_DIFF_THRESHOLD = 15 * np.pi / 180
