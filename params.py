b530c4977f3a33d4044d30ee54a1774f0f16a33a
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-15 18:00:27 -0400

    [params] switching heading sampling to be 0->2pi for easy sign keeping, added path cost params

--- params.py
+++ params.py
@@ -5,10 +5,15 @@ MAP_X_MIN = 0
 MAP_X_MAX = 10
 MAP_Y_MIN = 0
 MAP_Y_MAX = 10
-YAW_MIN = -np.pi
-YAW_MAX = np.pi
+YAW_MIN = 0
+YAW_MAX = 2*np.pi
 
 # steer function parameters
 MIN_STEP = 0.2
 MAX_STEP = 3
 YAW_DIFF_THRESHOLD = 15 * np.pi / 180
+PATH_LINSPACE_N = 5
+
+# path cost params
+EUCLIDEAN_DIST_COEFF = 1
+HEADING_DIFF_COEFF = 0.1
