fecabd599345f985017ab8e82caaa5e75a6f4398
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-16 01:31:18 -0400

    [params] reduced max step, set near dist thresh to max_step

--- params.py
+++ params.py
@@ -17,7 +17,7 @@ GOAL_BIAS = 0.05
 
 # steer function parameters
 MIN_STEP = 0.0
-MAX_STEP = 0.5
+MAX_STEP = 0.25
 YAW_DIFF_THRESHOLD = 15 * np.pi / 180
 PATH_LINSPACE_N = 5
 
@@ -26,8 +26,8 @@ EUCLIDEAN_DIST_COEFF = 1
 HEADING_DIFF_COEFF = 0.1
 
 # goal node checking params
-GOAL_DIST_THRESHOLD = 0.1
+GOAL_DIST_THRESHOLD = 0.2
 
 # RRT* params
 GAMMA_STAR = 5
-NEAR_DIST_THRESHOLD = 0.5  # can be at most MAX_STEP
+NEAR_DIST_THRESHOLD = MAX_STEP # can be at most MAX_STEP
