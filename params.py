02d6eeb80d22eba98c7e98fcc60795f2ad780ddd
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-15 22:23:44 -0400

    [params] added goal bias

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # global params
-N_NODES = 10
+N_NODES = 100
 
 # state plotting params
 ARROW_LENGTH = 0.2
@@ -13,13 +13,17 @@ MAP_Y_MIN = 0
 MAP_Y_MAX = 10
 YAW_MIN = 0
 YAW_MAX = 2*np.pi
+GOAL_BIAS = 0.05
 
 # steer function parameters
-MIN_STEP = 0.2
-MAX_STEP = 3
+MIN_STEP = 0.0
+MAX_STEP = 0.5
 YAW_DIFF_THRESHOLD = 15 * np.pi / 180
 PATH_LINSPACE_N = 5
 
 # path cost params
 EUCLIDEAN_DIST_COEFF = 1
 HEADING_DIFF_COEFF = 0.1
+
+# goal node checking params
+GOAL_DIST_THRESHOLD = 0.1
