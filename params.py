33aa4021363ccdd03daed0498c17892e4dc5b890
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-18 22:45:10 -0400

    [params] removed unused params

--- params.py
+++ params.py
@@ -1,29 +1,27 @@
 import numpy as np
 
 # global params
-N_NODES = 100
+N_NODES = 50
 
 # state plotting params
-ARROW_LENGTH = 0.2
+ARROW_LENGTH = 2
 
 # parameters for sampling nodes
 MAP_X_MIN = 0
-MAP_X_MAX = 10
+MAP_X_MAX = 100
 MAP_Y_MIN = 0
-MAP_Y_MAX = 10
-YAW_MIN = 0
-YAW_MAX = 2*np.pi
+MAP_Y_MAX = 100
+YAW_MIN = -np.pi
+YAW_MAX = np.pi
 GOAL_BIAS = 0.05
 
 # steer function parameters
-MIN_STEP = 0.0
-MAX_STEP = 0.25
-YAW_DIFF_THRESHOLD = 15 * np.pi / 180
-PATH_LINSPACE_N = 5
-
-# path cost params
-EUCLIDEAN_DIST_COEFF = 1
-HEADING_DIFF_COEFF = 0.1
+MIN_STEP = 2.0
+MAX_STEP = 5.0
+PATH_TIME_DURATION = 5
+PATH_TIME_DISCRETIZATION = 10
+PATH_INITIAL_SPEED = 1
+PATH_FINAL_SPEED = 1
 
 # goal node checking params
 GOAL_DIST_THRESHOLD = 0.2
