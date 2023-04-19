353fe921db8c1074f93916d3a6aec398f4738398
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-18 22:45:41 -0400

    [steer] updated steer to use new path gen and updated path cost

--- utilities.py
+++ utilities.py
@@ -9,32 +9,30 @@ def steer(near_node: XYThetaNode, rand_node: XYThetaNode):
     # if the nodes are too close together in the x-y plane, return early
     dist = near_node.dist_to_node(rand_node)
     if dist < MIN_STEP:
-        return None
+        return None, None
 
     near_node_coords = np.array(list(map(float, near_node.Point.coordinates))).reshape(-1, 1)
     rand_node_coords = np.array(list(map(float, rand_node.Point.coordinates))).reshape(-1, 1)
 
     # if the distance between nodes is too large, move as much as you can in the correct direction
-    dx = rand_node_coords[0] - near_node_coords[0]
-    dy = rand_node_coords[1] - near_node_coords[1]
+    dx = rand_node_coords[0, 0] - near_node_coords[0, 0]
+    dy = rand_node_coords[1, 0] - near_node_coords[1, 0]
     if dist > MAX_STEP:
         scale = float(MAX_STEP / dist)
         new_coords = near_node_coords + np.array([scale*dx, scale*dy, 0]).reshape(-1, 1)
+        new_coords[2, 0] = rand_node_coords[2, 0]
     else:
         new_coords = rand_node_coords
 
     # generate trajectory to new coordinates from near node and associated Curve object for collision checking
-    path, collision_object = generate_trajectory_function(near_node_coords, new_coords)
+    #print(f'near node yaw: {near_node_coords[2, 0]}')
+    #print(f'new node yaw: {new_coords[2, 0]}\n')
+    path, collision_objects = generate_trajectory_function(near_node_coords, new_coords)
 
-    # populate attributes of new_node
-    #new_node.parent = near_node
-    #new_node.cost_to_come = near_node.cost_to_come + path_cost(new_path)
-    #new_node.path_to_parent = (new_path, new_path_collision_object)
-
-    return path, collision_object
+    return path, collision_objects
 
 
-def path_cost(path: Curve):
-    return path.length
+def path_cost(path_collision_objects):
+    return float(sum([segment.length for segment in path_collision_objects]))
 
 
