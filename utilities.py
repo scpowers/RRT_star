db2e25611a29026e35ce518fae1a07d0f5cbc627
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-20 16:10:02 -0400

    [steer] hot-swapped generate_straight_path call instead of generate_trajectory

--- utilities.py
+++ utilities.py
@@ -1,8 +1,8 @@
 import numpy as np
-from params import MAX_STEP, MIN_STEP
+from params import MAX_STEP, MIN_STEP, GOAL_DIST_THRESHOLD
 from sympy import Curve
 from Node import XYThetaNode
-from OtherUtilities import generate_trajectory_function
+from OtherUtilities import generate_trajectory, generate_straight_path
 
 
 def steer(near_node: XYThetaNode, rand_node: XYThetaNode):
@@ -18,16 +18,19 @@ def steer(near_node: XYThetaNode, rand_node: XYThetaNode):
     dx = rand_node_coords[0, 0] - near_node_coords[0, 0]
     dy = rand_node_coords[1, 0] - near_node_coords[1, 0]
     if dist > MAX_STEP:
+        close_enough_to_goal = False
         scale = float(MAX_STEP / dist)
         new_coords = near_node_coords + np.array([scale*dx, scale*dy, 0]).reshape(-1, 1)
         new_coords[2, 0] = rand_node_coords[2, 0]
     else:
         new_coords = rand_node_coords
+        close_enough_to_goal = True
 
     # generate trajectory to new coordinates from near node and associated Curve object for collision checking
-    #print(f'near node yaw: {near_node_coords[2, 0]}')
-    #print(f'new node yaw: {new_coords[2, 0]}\n')
-    path, collision_objects, controls, valid_path = generate_trajectory_function(near_node_coords, new_coords)
+    #path, collision_objects, controls, valid_path = generate_trajectory(near_node_coords, new_coords)
+    new_coords_are_goal = rand_node.is_goal and close_enough_to_goal
+    path, collision_objects, controls, valid_path = generate_straight_path(near_node_coords, new_coords,
+                                                                           new_coords_are_goal)
 
     # this is hit if the generated controls are beyond the steering control limits
     if not valid_path:
