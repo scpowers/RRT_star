7cb4a396b5038da0b97f6d9a82c15852bee596f1
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-20 20:01:27 -0400

    [RRT] if new node from steer is too close to an existing one, return. also, add angle check to goal_solution check

--- RRT.py
+++ RRT.py
@@ -1,9 +1,10 @@
 from RRTBase import RRTBase
 from utilities import steer
 import numpy as np
-from params import GOAL_BIAS
+from params import GOAL_BIAS, MIN_STEP
 from Node import XYThetaNode
 from utilities import path_cost
+from OtherUtilities import angle_check
 
 
 class RRT(RRTBase):
@@ -27,13 +28,15 @@ class RRT(RRTBase):
 
         # finally create a new Node
         new_node = XYThetaNode(path[0, -1], path[1, -1], path[2, -1])
+        if new_node.dist_to_node(self.T[self.get_nearest_node_idx(new_node)]) < MIN_STEP:
+            return
         new_node.parent = nearest_node
         new_node.path_to_parent = (path, collision_objects)
         new_node.cost_to_come = nearest_node.cost_to_come + path_cost(collision_objects)
         new_node.yaw = path[2, -1]
 
         # set flag saying that a solution has been found if the new node from steer is close to the goal node
-        if self.goal_solution is None and self.is_close_to_goal(new_node):
+        if self.goal_solution is None and self.is_close_to_goal(new_node) and angle_check(self.goal.yaw, new_node.yaw):
             self.goal_solution = new_node
             new_node.is_goal = True
             new_node.yaw = self.goal.yaw
