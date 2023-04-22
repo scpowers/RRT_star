7dc1fba0ddc69052c4e0c6c5bc7deb4b786c46a2
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-22 15:11:35 -0400

    [RRTBase] further docstring improvements

--- RRTBase.py
+++ RRTBase.py
@@ -9,37 +9,41 @@ class RRTBase(ABC):
     Abstract base class for rapidly-exploring random trees.
 
     **Attribute**
-        T: **list, Node**
-            List of Node objects representing the tree
-        root: **Node**
-            Node object representing the root configuration
-        goal: **Node**
-            Node object representing the goal configuration
-        obs: **list, Obstacle**
-            List of Obstacle objects in the environment
-        goal_solution: **Node**
-            Node object representing the actual discovered goal node with a parent node and cost-to-come
+
+    T: **list, Node**
+        List of Node objects representing the tree
+    root: **Node**
+        Node object representing the root configuration
+    goal: **Node**
+        Node object representing the goal configuration
+    obs: **list, Obstacle**
+        List of Obstacle objects in the environment
+    goal_solution: **Node**
+        Node object representing the actual discovered goal node with a parent node and cost-to-come
 
     **Methods**
-        * add_node: abstract method that derived classes must implement to add a node to the tree.
-        * sample_free: return a random Node outside of any obstacles
-        * is_node_free: return True if the node is not witin any obstacles
-        * is_path_free: return True if the node's path to parent does not intersect any obstacles
-        * get_nearest_node_idx: get index of the node closest to the given node within the tree
-        * is_close_to_goal: return True if a given node is close enough to the goal to call them equivalent
-        * build_tree: abstract method that derived classes must implement to build the tree
-        * visualize_tree: plot the tree's nodes and edges in the environment with obstacles
+
+    * add_node: abstract method that derived classes must implement to add a node to the tree.
+    * sample_free: return a random Node outside of any obstacles
+    * is_node_free: return True if the node is not witin any obstacles
+    * is_path_free: return True if the node's path to parent does not intersect any obstacles
+    * get_nearest_node_idx: get index of the node closest to the given node within the tree
+    * is_close_to_goal: return True if a given node is close enough to the goal to call them equivalent
+    * build_tree: abstract method that derived classes must implement to build the tree
+    * visualize_tree: plot the tree's nodes and edges in the environment with obstacles
 
     """
     def __init__(self, q_start, q_goal, obs):
         """
         Constructor for RRT base class.
 
-        :param q_start: **list, float**
+        **Arguments**
+        
+        q_start: **list, float**
             state vector of the starting configuration
-        :param q_goal: **list, float**
+        q_goal: **list, float**
             state vector of the goal configuration
-        :param obs: **list, Obstacle**
+        obs: **list, Obstacle**
             list of Obstacle objects
         """
         root = XYThetaNode(q_start[0], q_start[1], q_start[2])
