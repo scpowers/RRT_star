397c2baea76e77a77f9d23bce69bf93e116f4f77
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-22 14:35:25 -0400

    [Node] updated docstrings

--- Node.py
+++ Node.py
@@ -9,20 +9,27 @@ class Node:
     """
     Base class for nodes.
 
-    ** Attributes **
-        Point: Point object corresponding to position in state space
-        parent: Node object that is this node's parent in the tree
-        cost_to_come: cost to travel to this node via its parents from the root
-        path_to_parent: array(s) of points describing the trajectory from the parent to this node in state space
+    **Attributes**
+        Point: **sympy Point**
+            Point object corresponding to position in state space
+        parent: **Node**
+            Node object that is this node's parent in the tree
+        cost_to_come: **float or int**
+            cost to travel to this node via its parents from the root
+        path_to_parent: **numpy array**
+            array(s) of points describing the trajectory from the parent to this node in state space
 
-    ** Methods **
+    **Methods**
         dist_to_node: get Euclidean distance to a given Node
+        plot_node: plot the node on the current Matplotlib figure
     """
     def __init__(self, states):
         """
-        Parameter constructor for general node.
+        Constructor for general node.
 
-        Args:
+        **Args**
+            states: **list, float**
+                state vector corresponding to this node
         """
         assert len(states) >= 2, "Must have at least two state variables"
         self.Point = Point(states)
@@ -32,39 +39,49 @@ class Node:
         self.plotting_object = np.array(states)
 
     def dist_to_node(self, node):
+        """Compute Euclidean distance between this node and a given node"""
         (x1, y1) = map(float, self.Point.coordinates)
         (x2, y2) = map(float, node.Point.coordinates)
         return fast_Euclidean_dist(x1, y1, x2, y2)
 
     def plot_node(self):
+        """Plot this node in the current Matplotlib figure"""
         pass
 
 
 class XYThetaNode(Node):
     """
-    class for x-y-theta nodes.
+    Class for x-y-theta nodes.
 
-    ** Attributes **
-        Point: Point object corresponding to x-y position
-        yaw: heading in x-y plane
-        parent: Node object that is this node's parent in the tree
-        cost_to_come: cost to travel to this node via its parents from the root
-        path_to_parent: array(s) of points describing the trajectory from the parent to this node in state space
+    **Attributes**
+        Point: **sympy Point**
+            Point object corresponding to position in state space
+        parent: **Node**
+            Node object that is this node's parent in the tree
+        cost_to_come: **float or int**
+            cost to travel to this node via its parents from the root
+        path_to_parent: **numpy array**
+            array(s) of points describing the trajectory from the parent to this node in state space
+        yaw: **float**
+            Heading corresponding to this state vector
+        is_goal: **bool**
+            True if this node is a goal node
 
-    ** Methods **
+    **Methods**
         dist_to_node: get distance metric to a given Node
+        plot_node: plot the node on the current Matplotlib figure
     """
     def __init__(self, x=None, y=None, theta=None):
         """
         Constructor for x-y-theta node.
 
         Args:
-            x: **double or int**
+            x: **double or int, optional**
                 x-coordinate of vehicle
-            y: **double or int**
+            y: **double or int, optional**
                 y-coordinate of vehicle
-            theta: **double or int**
-                heading of vehicle on x-y plane (in radians)
+            theta: **double or int, optional**
+                Heading of vehicle on x-y plane (in radians)
         """
         if x is None:
             x = np.random.uniform(MAP_X_MIN, MAP_X_MAX)
@@ -83,15 +100,10 @@ class XYThetaNode(Node):
         return fast_Euclidean_dist(x1, y1, x2, y2)
 
     def plot_node(self):
+        """Plot this node in the current Matplotlib figure"""
         if self.is_goal:
             plt.arrow(self.plotting_object[0], self.plotting_object[1],
                       ARROW_LENGTH * np.cos(self.yaw), ARROW_LENGTH * np.sin(self.yaw), width=ARROW_WIDTH, facecolor='m')
         else:
             plt.arrow(self.plotting_object[0], self.plotting_object[1],
                       ARROW_LENGTH * np.cos(self.yaw), ARROW_LENGTH * np.sin(self.yaw), width=ARROW_WIDTH, facecolor='g')
-        """
-        if self.is_goal:
-            plt.plot(self.plotting_object[0], self.plotting_object[1], color='m', marker='o')
-        else:
-            plt.plot(self.plotting_object[0], self.plotting_object[1], color='g', marker='.')
-        """
