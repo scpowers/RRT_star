e78e42f450324c927321394287e7384545e1b4c0
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-24 22:14:14 -0400

    [run] cleaned up example usage

--- run.py
+++ run.py
@@ -2,15 +2,20 @@ from Obstacle import *
 from RRTStar import RRTStar
 from RRT import RRT
 
-circ_obs = CircularObstacle(40, 20, 10)
-rect_obs = PolygonObstacle((30, 0), (40, 0), (40, 10), (30, 10))
-circ2_obs = CircularObstacle(90, 90, 5)
+if __name__ == '__main__':
+    # create Obstacle objects
+    circ_obs = CircularObstacle(40, 20, 10)
+    rect_obs = PolygonObstacle((60, 60), (75, 65), (85, 80), (70, 75))
 
-q_start = [0, 0, 0]
-q_goal = [60, 40, 0]
-#obs = [circ_obs, rect_obs, circ2_obs]
-obs = [circ_obs]
-#obs = []
-rrt = RRT(q_start, q_goal, obs)
-rrt.build_tree()
-rrt.visualize_tree()
+    # specify initial and goal poses
+    q_start = [0, 0, 0]
+    q_goal = [60, 40, 0]
+
+    # initialize tree
+    obs = [circ_obs, rect_obs]
+    rrt = RRT(q_start, q_goal, obs)
+    #rrt = RRTStar(q_start, q_goal, obs)
+
+    # build and visualize the tree
+    rrt.build_tree()
+    rrt.visualize_tree()
