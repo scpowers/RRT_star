b9504ed4d59163acb13c3a0dc4948ea84d270fe7
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-04-30 14:35:13 -0400

    [run] cleaned up example run file

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from Obstacle import *
-from RRTStar import RRTStar
 from RRT import RRT
+import numpy as np
 
 if __name__ == '__main__':
     # create Obstacle objects
@@ -9,12 +9,11 @@ if __name__ == '__main__':
 
     # specify initial and goal poses
     q_start = [0, 0, 0]
-    q_goal = [60, 40, 0]
+    q_goal = [50, 45, np.pi/6]
 
     # initialize tree
     obs = [circ_obs, rect_obs]
     rrt = RRT(q_start, q_goal, obs)
-    #rrt = RRTStar(q_start, q_goal, obs)
 
     # build and visualize the tree
     rrt.build_tree()
